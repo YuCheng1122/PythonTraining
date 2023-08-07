84dc511e9f306b4600d55cb60ae158026f628963
YuCheng1122 <tommymask00@gmail.com>
2023-08-07 23:48:06 +0800

    Chapter13

--- Chapter12 - Class/12-7.py
+++ Chapter12 - Class/12-7.py
@@ -15,4 +15,28 @@ class Name:
         return f'{self.name}'
     __repr__ =__str__
 
-#__iter__() 迭代物件
+#__iter__() 迭代物件，類似於list 或 tuple，提供給for....in 循環內使用，這時類別需要next()方法取得下一個值。
+class Fib():
+    def __init__(self,max) -> None:
+        self.max=max
+    def __iter__(self):
+        self.a = 0
+        self.b = 1
+        return self
+    def __next__(self):
+        fib = self.a
+        if fib > self.max:
+            raise StopIteration
+        self.a , self.b = self.b, self.a + self.b
+        return fib
+for i in Fib(100):
+    print(i)
+# __eq__() Method __eq__() == equals
+class City():
+    def __init__(self, name) -> None:
+        self.name = name
+    def equals(self, city2):
+        return self.name.upper() == city2.name.upper()
+    def __eq__(self, city2):
+        return self.name.upper() == city2.name.upper()
+    
